f03a4e0a3fe85f93daacb4cac56029400872e77a
Nick Ye <nick.ye888@gmail.com>
2018-02-08 22:43:55 +1300

    added input box

---
+++ my-app/src/TodoBox/TodoBox.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import TodoEntry from "./TodoEntry";
+
+class TodoBox extends Component {
+    render() {
+        return (
+            <TodoEntry/>
+        );
+    }
+}
+
+export default TodoBox;
---
+++ my-app/src/TodoBox/TodoEntry.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class TodoEntry extends Component {
+    render() {
+        return (
+            <input/>
+        );
+    }
+}
+
+export default TodoEntry;
