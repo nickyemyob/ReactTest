f072615711093f464de2afa81cade0a33fdc7542
Nick Ye <nick.ye888@gmail.com>
2018-02-09 23:07:09 +1300

    created basic structure

--- my-app/src/TodoBox/TodoBox.jsx
+++ my-app/src/TodoBox/TodoBox.jsx
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
-import TodoEntry from "./TodoEntry";
+import TodoEntry from "./TodoInput";
+import TodoList from './TodoList/TodoList';
 
 class TodoBox extends Component {
     render() {
         return (
-            <TodoEntry/>
+            <div>
+                <TodoEntry/>
+                <TodoList/>
+            </div>
         );
     }
 }
---
+++ my-app/src/TodoBox/TodoList/TodoRecord.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class TodoRecord extends Component {
+    render() {
+        return (
+            <div className="row">
+                <label>hello</label>
+                <button>remove</button>
+            </div>
+        )
+    }
+}
+
+export default TodoRecord;
